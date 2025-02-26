3db7a22c1da213d34656537e3ade7a57479fbe31
aceaspades <aceaspades@protonmail.com>
2025-02-26 14:42:20 -0800

    Moved type tag to optional

--- src/types/NostrProduct.ts
+++ src/types/NostrProduct.ts
@@ -1,7 +1,9 @@
 
 type ShippingTag = ['shipping', `30406:${string}:${string}`]; // Shipping method; follows addressable format of "30406:<pubkey>:<d-tag>"
 type ShippingCollectionTag = ['shipping', `30405:${string}:${string}`]; // References to a product collection, in this case, shipping is inherited from the collection; follows addressable format of "30405:<pubkey>:<d-tag>"
-type OptionalTag = string[] | ShippingTag | ShippingCollectionTag;
+type TypeTag = ['type', string]; // ["type", "simple" | "variable" | "variation"]
+
+type OptionalTag = string[] | ShippingTag | ShippingCollectionTag | TypeTag;
 
 export type NostrProduct = {
     kind: 30402,
@@ -9,7 +11,6 @@ export type NostrProduct = {
         ['d', string], // Medusa Product ID
         ['title', string],
         ['price', string, string], // ["price", <amount>, <currency>]
-        ['type', string], // ["type", "simple" | "variable" | "variation"]
         ...([OptionalTag] | OptionalTag[]) // Additional optional tags (zero or more)
     ],
     content: string // Note to the Merchant
